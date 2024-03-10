63d673318939f1c5363ada2a7958bd405e53118d
tom tait <thomas.t.ca@outlook.com>
2024-03-10 14:17:21 -0300

    projects

---
+++ index.js
@@ -0,0 +1,24 @@
+document.getElementById("linkedin").onclick = function() {
+    location.href = "https://www.linkedin.com/in/tom-tait/"
+}
+document.getElementById("github").onclick = function() {
+    location.href = "https://github.com/t0mtait"
+}
+document.getElementById("encryptorView").onclick = function() {
+    location.href = "https://encryptor.tomtait.dev"
+}
+document.getElementById("encryptorGit").onclick = function() {
+    alert("private repository. public coming soon!")
+}
+document.getElementById("passwordView").onclick = function() {
+    location.href = "https://passwordgenerator.tomtait.dev"
+}
+document.getElementById("passwordGit").onclick = function() {
+    location.href = "https://github.com/t0mtait/PasswordGenerator"
+}
+document.getElementById("simonView").onclick = function() {
+    location.href = "https://simon.tomtait.dev"
+}
+document.getElementById("simonGit").onclick = function() {
+    location.href = "https://github.com/t0mtait/simon"
+}
